35469775da8414c1e761f0a8c743817dff323e57
Multinecz <nicolas.cepeda@alumnos.upla.cl>
2023-04-26 15:42:52 -0400

    Clase inicial

---
+++ 2.- Variables.py
@@ -0,0 +1,14 @@
+numero = 3
+print(numero)
+
+numero1 = 5
+numero2 = 3
+
+print(numero1 + numero2)
+
+# Reglas del nombre de las variables
+# 1.- No pueden empezar por un número
+# 2.- Recomendación, escribir siempre en minúscula
+# 3.- Separar las palabras mediante un guión bajo
+# 4.- No se puede escribir espacio
+# 5.- No pueden contener carácteres especiales
